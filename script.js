44e93b27c43ce506d46a6ab75d8efb6a4af6232e
Rerovi <rerovi-rev@outlook.com>
2021-02-12 13:30:09 +0100

    Dynamic let
    Made most of the questions dynamically available

--- script.js
+++ script.js
@@ -9,48 +9,60 @@ const titelTekst = document.getElementById("vraag-tekst");
 
 let answerArr = [0, 0, 0];
 let answerCount = 0;
+let answerObj = {};
+
+let count = Object.keys(subjects).length;
+
+for (let i = 0; i < count; i++) {
+    answerObj[i + 1] = "";
+}
+
+console.log(answerObj);
 
 eensBttn.onclick = eens;
 geenBttn.onclick = geen;
 oneensBttn.onclick = oneens;
-overslaBttn.onclick = overslaan;
+overslaBttn.onclick = volgendeVraag;
 terugBttn.onclick = vraagTerug;
 
+geenBttn.style.display = "none";
+oneensBttn.style.display = "none";
+overslaBttn.style.display = "none";
+
 function eens() {
-    console.log("Vraag eens");
-    answerArr[0]++;
-    console.log(answerArr);
+    if (answerCount == 0) {
+        eensBttn.innerHTML = "Eens";
+        geenBttn.style.display = "inline-block";
+        oneensBttn.style.display = "inline-block";
+        overslaBttn.style.display = "inline-block";
+    } else {
+    }
+    answerObj[answerCount] = "eens";
+    console.log(answerObj);
     volgendeVraag();
 }
 
 function geen() {
-    console.log("Vraag geen van beide");
-    answerArr[1]++;
-    console.log(answerArr);
+    answerObj[answerCount] = "geen";
+    console.log(answerObj);
     volgendeVraag()
 }
 
 function oneens() {
-    console.log("Vraag is oneens");
-    answerArr[2]++;
-    console.log(answerArr);
-    volgendeVraag();
-}
-
-function overslaan() {
-    console.log("Vraag overgeslagen");
+    answerObj[answerCount] = "oneens";
+    console.log(answerObj);
     volgendeVraag();
 }
 
 function volgendeVraag() {
     answerCount++;
-    console.log("volgende vraag");
-    for (var i = 0; i < subjects.length; i++) {
-        if (answerCount-1 == i) {
-            titel.innerHTML = answerCount+". "+subjects[answerCount-1].title;
-            titelTekst.innerHTML = subjects[answerCount-1].statement;
-        } else if(answerCount == subjects.length+1){
+    for (let i = 0; i < count; i++) {
+        if (answerCount - 1 == i) {
+            titel.innerHTML = answerCount + ". " + subjects[answerCount - 1].title;
+            titelTekst.innerHTML = subjects[answerCount - 1].statement;
+        } else if (answerCount == count + 1) {
             alert("dat waren alle vragen");
+            answerArr = [0, 0, 0];
             answerCount = 0;
             titel.innerHTML = "";
             titelTekst.innerHTML = "";
@@ -59,6 +71,9 @@ function volgendeVraag() {
 }
 
 function vraagTerug() {
+    answerCount--;
     console.log("vraag terug");
+    titel.innerHTML = answerCount + ". " + subjects[answerCount - 1].title;
+    titelTekst.innerHTML = subjects[answerCount - 1].statement;
 }
 
