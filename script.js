bd24df16029d09cea5f162d38bfe8b1234af3dd5
Rerovi <rerovi-rev@outlook.com>
2021-02-16 09:51:49 +0100

    Result function
    Finished the function which "calculates" the percentages of all the politicial parties

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const oneensBttn = document.getElementById("oneens");
 const overslaBttn = document.getElementById("overslaan");
 const terugBttn = document.getElementById("terug");
 const buttons = document.getElementsByClassName("button");
+const progressieBalk = document.getElementById("progressie");
 
 const titel = document.getElementById("vraag");
 const titelTekst = document.getElementById("vraag-tekst");
@@ -33,6 +34,7 @@ function eens() {
         geenBttn.style.display = "inline-block";
         oneensBttn.style.display = "inline-block";
         overslaBttn.style.display = "inline-block";
+        progressieBalk.style.display = "inline-block";
     } else {
         answerObj[answerCount] = "eens";
     }
@@ -60,10 +62,9 @@ function volgendeVraag() {
     answerCount++;
     for (let i = 0; i < count; i++) {
          if (answerCount == count + 1){
-             alert("dat waren alle vragen");
-             answerCount = 0;
-             titel.innerHTML = "";
-             titelTekst.innerHTML = "";
+             overslaBttn.style.display = "none";
+             partijBerekening();
+             break
         }  else if (answerCount - 1 == i) {
              titel.innerHTML = answerCount + ". " + subjects[answerCount - 1].title;
              titelTekst.innerHTML = subjects[answerCount - 1].statement;
@@ -97,4 +98,31 @@ function vraagTerug() {
         }
     }
 }
+
+function partijBerekening() {
+    console.log("test");
+    console.log(answerObj);
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.display = "none";
+    }
+    console.log(answerObj[1]);
+    if (answerObj[1] == "oneens" && answerObj[2] == "eens" && answerObj[3] == "oneens" && answerObj[4] == "eens") {
+        console.log("PVV");
+        titel.innerHTML = "Uw uitslag is:";
+        titelTekst.innerHTML = "100% PVV <br> 25% D66 <br> 25% SP <br> 25% CU";
+    } else if (answerObj[1] == "eens" && answerObj[2] == "oneens" && answerObj[3] == "oneens" && answerObj[4] == "oneens") {
+        console.log("SP");
+        titel.innerHTML = "Uw uitslag is:";
+        titelTekst.innerHTML = "100% SP <br> 25% D66 <br> 25% PVV <br> 25% CU";
+    } else if (answerObj[1] == "eens" && answerObj[2] == "oneens" && answerObj[3] == "eens" && answerObj[4] == "oneens") {
+        console.log("D66");
+        titel.innerHTML = "Uw uitslag is:";
+        titelTekst.innerHTML = "100% D66 <br> 25% SP <br> 25% PVV <br> 25% CU";
+    } else if (answerObj[1] == "eens" && answerObj[2] == "eens" && answerObj[3] == "eens" && answerObj[4] == "oneens") {
+        console.log("CU");
+        titel.innerHTML = "Uw uitslag is:";
+        titelTekst.innerHTML = "100% CU <br> 25% D66 <br> 25% PVV <br> 25% SP";
+    }
+}
+
 
