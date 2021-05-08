69badf2ed2e2d5657dbe81d7f1e62186d9390465
Rerovi <rerovi-rev@outlook.com>
2021-05-08 14:55:07 +0200

    Finished
    Finished everything from the trello board.

--- script.js
+++ script.js
@@ -13,16 +13,19 @@ const primairBox = document.getElementById("primair");
 const secundairBox = document.getElementById("secundair");
 
 let count = Object.keys(subjects).length;
+let stop = true;
 let answerCount = 0;
 let answerObj = {};
+let weging = {};
 let score = {};
 let sort = [];
+var vraagItems = [];
+var lijstLabel = [];
 let check;
 
-geenBttn.style.display = "none";
-oneensBttn.style.display = "none";
-overslaBttn.style.display = "none";
-terugBttn.style.display = "none";
+for (let g = 1; g < subjects.length; g++) {
+
+}
 
 oneensBttn.onclick = () => antwoord('contra');
 geenBttn.onclick = () => antwoord('none');
@@ -51,6 +54,27 @@ function partijSelectie() {
     console.log(score);
 }
 
+function vraagWegingen() {
+    stop = false;
+    titel.innerHTML = "Zijn er nog onderwerpen die u belangrijk vindt?";
+    titelTekst.innerHTML = "Aangevinkte instellingen tellen extra mee bij het berekenen van uw resultaat.";
+    for (let i = 0; i < Object.keys(subjects).length; i++) {
+        lijstLabel[i] = document.createElement("label");
+        vraagItems[i] = document.createElement("INPUT");
+        lijstLabel[i].for = i;
+        lijstLabel[i].innerHTML = subjects[i].title;
+        vraagItems[i].name = subjects[i].title;
+        vraagItems[i].id = i;
+        vraagItems[i].type = "checkbox";
+
+        lijst.appendChild(vraagItems[i]);
+        lijst.appendChild(lijstLabel[i]);
+        lijst.appendChild(document.createElement("BR"));
+
+        vraagItems[i].innerHTML = subjects[i].title;
+    }
+}
+
 function knopKleur() {
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.backgroundColor = "gray";
@@ -64,60 +88,86 @@ function knopKleur() {
     }
 }
 
+
+
 function antwoord(antwoord) {
     if (primairBox.checked || secundairBox.checked) {
-        partijSelectie();
-        if (antwoord == "overslaan") {
-            answerObj[answerCount - 1] = "";
-            console.log("overslaan");
-            answerCount++;
-            console.log(answerCount);
-        } else {
-            if (answerCount == 0) {
-                answerCount++;
-                eensBttn.innerHTML = "Eens";
-                lijst.innerHTML = "";
-                geenBttn.style.display = "inline-block";
-                oneensBttn.style.display = "inline-block";
-                overslaBttn.style.display = "inline-block";
-                terugBttn.style.display = "inline-block";
-            } else {
-                knopKleur();
-                answerObj[answerCount - 1] = antwoord;
-                answerCount++;
-                console.log(answerObj);
-                console.log(answerCount);
+        lijst.innerHTML = "";
+        if (stop == true) {
+            vraagWegingen();
+        } else if (stop == false) {
+            for (let i = 0; i < vraagItems.length; i++) {
+                if (vraagItems[i].checked) {
+                    weging[i] = 2;
+                } else {
+                    weging[i] = 1;
+                }
             }
-        }
-        if (answerCount == count + 1) {
-            partijBerekening();
-            eindscherm();
-            console.log(answerObj);
-            console.log(answerCount);
+            console.log(weging);
+            partijSelectie();
+            if (stop == true) {
+            } else if (stop == false) {
+                if (antwoord == "overslaan") {
+                    answerObj[answerCount - 1] = "";
+                    console.log("overslaan");
+                    answerCount++;
+                    console.log(answerCount);
+                } else {
+                    if (answerCount == 0) {
+                        answerCount++;
+                        eensBttn.innerHTML = "Eens";
+                        lijst.innerHTML = "";
+                        geenBttn.style.display = "inline-block";
+                        oneensBttn.style.display = "inline-block";
+                        overslaBttn.style.display = "inline-block";
+                        terugBttn.style.display = "inline-block";
+                    } else {
+                        knopKleur();
+                        answerObj[answerCount - 1] = antwoord;
+                        answerCount++;
+                        console.log(answerObj);
+                        console.log(answerCount);
+                    }
+                }
+                if (answerCount == count + 1) {
+                    partijBerekening();
+                    eindscherm();
+                    console.log(answerObj);
+                    console.log(answerCount);
+                } else {
+                    titel.innerHTML = answerCount + ". " + subjects[answerCount - 1].title;
+                    titelTekst.innerHTML = subjects[answerCount - 1].statement;
+                    knopKleur();
+                }
+            }
         } else {
-            titel.innerHTML = answerCount + ". " + subjects[answerCount - 1].title;
-            titelTekst.innerHTML = subjects[answerCount - 1].statement;
-            knopKleur()
+            alert("U moet 1 optie kiezen");
         }
-    } else {
-        alert("U moet minstens 1 optie aanklikken!");
     }
 }
 
 function vraagTerug() {
-    answerCount--;
-    console.log(answerCount);
-    titel.innerHTML = answerCount + ". " + subjects[answerCount - 1].title;
-    titelTekst.innerHTML = subjects[answerCount - 1].statement;
-    knopKleur();
+    if (answerCount = 1) {
+        let beginvraag = confirm("Dit is de eerste vraag, weet u zeker dat u wilt stoppen?");
+        if (beginvraag == true) {
+            location.reload();
+        } else {
+        }
+    } else {
+        answerCount--;
+        console.log(answerCount);
+        titel.innerHTML = answerCount + ". " + subjects[answerCount - 1].title;
+        titelTekst.innerHTML = subjects[answerCount - 1].statement;
+        knopKleur();
+    }
 }
 
 function partijBerekening() {
     for (let i = 0; i < subjects.length; i++) {
         for (let j = 0; j < subjects[i].parties.length; j++) {
             if (answerObj[i] == subjects[i].parties[j].position) {
+                score[parties[j].name] = weging[i];
                 console.log(score);
-                score[subjects[i].parties[j].name]++;
             }
         }
     }
@@ -149,7 +199,7 @@ function verwijder() {
 function eindscherm() {
     sorteerObject();
     verwijder();
-    var lijstItems = [];
+    let lijstItems = [];
     titel.innerHTML = "Uitslagen!";
     for (let i = 0; i < Object.keys(sort).length; i++) {
         if (Number.isNaN(sort[i][1])) {
@@ -160,5 +210,8 @@ function eindscherm() {
         lijstItems[i].innerHTML = sort[i][0] + " " + sort[i][1] + "%";
     }
 }
+
+
+
 
 
